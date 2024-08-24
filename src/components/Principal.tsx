db8b8b9a988d45d6931a88dcb09c4dcd5d1ed5cd
JulianV122 <julivelo69@gmail.com>
2024-08-24 17:46:49 -0500

    Modificacion del aside

--- src/components/Principal.tsx
+++ src/components/Principal.tsx
@@ -4,7 +4,15 @@ export default function Principal() {
     return (
         <main className="main">
             <aside className="aside">
-                This is the aside
+                <div className="collection">
+                    <img src="src\assets\collection.png" alt="Collection" />
+                </div>
+                <div className="like">
+                    <img src="src\assets\like.png" alt="Like" />
+                </div>
+                <div className="history">
+                    <img src="src\assets\history.png" alt="History" />
+                </div>
             </aside>
             <section className="principal">
                 This is the principal content
