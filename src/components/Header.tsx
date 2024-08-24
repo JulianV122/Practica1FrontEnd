56964f886b4d977a3eb8f4c6f699f2c420628b9e
JulianV122 <julivelo69@gmail.com>
2024-08-24 00:10:11 -0500

    Añadidos iconos y estilos del header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,21 @@ import '../styles/Header.css'
 export default function header() {
     return (
         <header className="header">
-            This is the header
+            <div className="home">
+                <img src="src\assets\home.png" alt="Home" />
+            </div>
+            <div className="lupa">
+                <img src="src\assets\lupa.png" alt="Lupa" />
+            </div>
+            <div className="barra">
+                Que deseas buscar?
+            </div>
+            <div className="user">
+                <img src="src\assets\user.png" alt="Usuario" />
+            </div>
+            <div className="config">
+                <img src="src\assets\config.png" alt="Configuraciones" />
+            </div>
         </header>
     )
 }
