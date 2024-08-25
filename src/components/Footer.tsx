cc00432718dfb31ac6449278fc7b4e302e9c6e1a
JulianV122 <julivelo69@gmail.com>
2024-08-25 00:02:06 -0500

    Modificaciones del footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,20 @@ import '../styles/Footer.css'
 export default function Footer() {
     return (
         <footer className="footer">
-            This is the footer
+            <div className="title">
+                <p> Nuestras redes sociales: </p>
+            </div>
+            <div className="icons">
+                <div className="github">
+                    <img src="src\assets\github.png" alt="" />
+                </div>
+                <div className="instagram">
+                    <img src="src\assets\instagram.png" alt="" />
+                </div>
+                <div className="x">
+                    <img src="src\assets\x.png" alt="" />
+                </div>
+            </div>
         </footer>
     )
 }
